178f50778bdaeea4ab0ea476f3cacaee5c34d037
Theo Beda <theo.github@bedafamily.com>
2021-07-02 23:52:21 -0700

    fix example context options on mac

--- examples/basic-usage/main.cpp
+++ examples/basic-usage/main.cpp
@@ -1,10 +1,10 @@
 #define LIBGLPLAYGROUND_DEFINE_MAIN
 #define LIBGLPLAYGROUND_APP_NAMESPACE basic_usage
 #include <libglplayground.h>
-#if defined(SYSTEM_MACOSX) && !defined(NDEBUG)
-constexpr bool mesa_context = true;
+#if !defined(SYSTEM_MACOSX) && !defined(NDEBUG)
+constexpr int32_t major_opengl_version = 4;
 #else
-constexpr bool mesa_context = false;
+constexpr int32_t major_opengl_version = 3;
 #endif
 using namespace libplayground::gl;
 namespace basic_usage {
@@ -13,7 +13,7 @@ namespace basic_usage {
     };
     class basic_usage_app : public application {
     public:
-        basic_usage_app() : application("Example application", 800, 600, mesa_context, 4) { }
+        basic_usage_app() : application("Example application", 800, 600, false, major_opengl_version) { }
     protected:
         virtual void load_content() override {
             std::vector<vertex_attribute> attributes = {
