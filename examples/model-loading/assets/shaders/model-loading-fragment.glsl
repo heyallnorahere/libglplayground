ecf26bf5cd8289d3d4a4d518f33295c9ab3daddd
yodasoda1219 <theo.github@bedafamily.com>
2021-07-07 11:57:47 -0700

    this is stupid

--- examples/model-loading/assets/shaders/model-loading-fragment.glsl
+++ examples/model-loading/assets/shaders/model-loading-fragment.glsl
@@ -3,5 +3,5 @@ out vec4 out_color;
 in vec2 uv;
 // todo: materials
 void main() {
-    out_color = vec4(1.0); // todo: calculate material stuff
+    out_color = vec4(0.0, 1.0, 0.0, 1.0); // todo: calculate material stuff
 }
